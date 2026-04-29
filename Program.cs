df43e813cfb14cbbaab0335adcaf19d21fcbe70b
Alexandre Gomes de Araujo <alexandre.araujo@leaf.com.br>
2026-04-29 11:33:31 -0300

    Implementa abstração de smartphones

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("11999999999", "Tijolão", "111111111111111", 128);
+Smartphone iphone = new Iphone("11888888888", "iPhone 15", "222222222222222", 256);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
